a44ced98051201ff71eef5f29c10c90d52e17c06
fajrinanurhaliza <fajrinanurhaliza7@gmail.com>
2026-05-20 16:05:12 +0700

    update

--- duckcare/lib/app/modules/duck_management/bindings/duck_management_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import '../controllers/duck_management_controller.dart';
-
-class DuckManagementBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<DuckManagementController>(() => DuckManagementController());
-  }
-}
